618a0a6b625bdeec7e3b3933d652992d7de5152e
pleasematheus <matheus4k@gmail.com>
2024-06-21 14:51:46 -0400

    Finished UsuarioController

--- src/controller/UsuarioController.ts
+++ src/controller/UsuarioController.ts
@@ -13,4 +13,27 @@ export class UsuarioController {
     const idInsercao: number = await createUser(usuario)
     return idInsercao
   }
+
+  static async atualizarUsuario(usuario: Usuario): Promise<void> {
+    await updateUser(usuario)
+  }
+
+  static async removerUsuario(usuarioId: number): Promise<void> {
+    await deleteUser(usuarioId)
+  }
+
+  static async buscarUsuarioPorId(usuarioId: number): Promise<Usuario | null> {
+    const usuario = await getUserById(usuarioId)
+    return usuario
+  }
+
+  static async listarTodosOsUsuarios(): Promise<Usuario[]> {
+    const usuarios = await listAllUsers()
+    return usuarios
+  }
+
+  static async listarUsuariosPorNome(usuario: string): Promise<Usuario[] | null> {
+    const usuarios = await listUsersByName(usuario)
+    return usuarios
+  }
 }
