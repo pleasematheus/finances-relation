114da1760f608d85171bea6e9e98b0279e432557
pleasematheus <matheus4k@gmail.com>
2024-06-21 14:59:58 -0400

    Finished ReceitaController

--- src/controller/ReceitaController.ts
+++ src/controller/ReceitaController.ts
@@ -1,6 +1,5 @@
 import { Receita } from "../model/RevenueModel"
 import {
-  connect,
   createRevenue,
   getRevenueById,
   updateRevenue,
@@ -8,5 +7,22 @@ import {
 } from '../Database'
 
 export class RevenueController {
-  
+  static async cadastrarReceita(receita: Receita): Promise<number> {
+    const idInsercao: number = await createRevenue(receita)
+    return idInsercao
+  }
+
+  static async buscarReceitaPorId(receitaId: number): Promise<Receita | null> {
+    const receita = await getRevenueById(receita)
+    return receita
+  }
+
+  static async atualizarReceita(receita: Receita): Promise<void> {
+    await updateRevenue(receita)
+  }
+
+  static async listarTodasAsReceitas(): Promise<Receita[]> {
+    const receitas = await listAllRevenues()
+    return receitas
+  }
 }
